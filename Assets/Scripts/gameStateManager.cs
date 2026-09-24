e7aa5070e19eb5cc2df8d6c7be84aaf42d856e2d
Bryce <bryce.h.bernstein@gmail.com>
2026-09-24 07:31:25 -0500

    Adding requested changes to bathroom

--- Assets/Scripts/gameStateManager.cs
+++ Assets/Scripts/gameStateManager.cs
@@ -39,4 +39,9 @@ public class GameStateManager : MonoBehaviour {
 		solvedRooms.Remove(room);
 		Debug.Log(room + " marked as unsolved");
 	}
+
+	public void SolveBathroom()
+	{
+		MarkSolved(Room.Bathroom);
+	}
 }
